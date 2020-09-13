aa29f748980d40b5a22ab9ee7b614779fd386e43
Lorenzosutera <sutera.lorenzo.1h@gmail.com>
2020-09-13 22:27:04 +0200

    Train definition changed based on the test and tests for car and train. Train need some refactor

--- src/main/scala/pps/covid_sim/model/transports/PublicTransports.scala
+++ src/main/scala/pps/covid_sim/model/transports/PublicTransports.scala
@@ -1,6 +1,6 @@
 package pps.covid_sim.model.transports
 
-import pps.covid_sim.model.people.PeopleGroup.Single
+import pps.covid_sim.model.people.PeopleGroup.{Group, Single}
 import pps.covid_sim.model.places.Locations.Location
 import pps.covid_sim.util.time.HoursInterval
 
@@ -29,6 +29,7 @@ object PublicTransports {
   case class Carriage(override val capacity: Int) extends Transport {
 
     def getCurrentPeopleInCarriage: Int = _numCurrentPeople
+    def getPeopleSet: Set[Group] = _peopleSet
 
   }
 
@@ -39,19 +40,30 @@ object PublicTransports {
     val carriageList: Seq[Carriage] = (1 to carriages) map (_ => Carriage(carriageCapacity))
     override val capacity: Int = carriageCapacity * carriages
 
+    ///TODO Refactor
     override def enter(person: Single): Option[Transport] = {
-      val ci = (carriageList map (c => c.getCurrentPeopleInCarriage)).zipWithIndex.min._2
-      if (carriageList(ci).getCurrentPeopleInCarriage < carriageCapacity && !_peopleSet.contains(person)) {
-        _numCurrentPeople += 1
-        _peopleSet += person
-        carriageList(ci).enter(person)
-      } else {
+      if (_numCurrentPeople < capacity && !_peopleSet.contains(person)) {
+        val ci = (carriageList map (c => c.getCurrentPeopleInCarriage)).zipWithIndex.min._2
+        //if (carriageList(ci).getCurrentPeopleInCarriage < carriageCapacity && !_peopleSet.contains(person)) {
+          _numCurrentPeople += 1
+          _peopleSet += person
+          carriageList(ci).enter(person)
+        //} else {
+          //None
+        //}
+      }else{
         None
       }
     }
 
     override def exit(person: Single): Unit = {
       if (_peopleSet.contains(person)) {
+        carriageList.foreach(c => {
+          if(c.getPeopleSet.contains(person)){
+            c.exit(person)
+          }
+        })
+
         _numCurrentPeople -= 1
         _peopleSet -= person
       }
--- src/test/scala/pps/covid_sim/model/transports/TransportsTest.scala
+++ src/test/scala/pps/covid_sim/model/transports/TransportsTest.scala
@@ -1,23 +1,27 @@
 package pps.covid_sim.model.transports
 
+import java.util.logging.LogRecord
+
 import org.junit.Assert._
 import org.junit.Test
 import pps.covid_sim.model.people.PeopleGroup.Single
 import pps.covid_sim.model.people.Person
 import pps.covid_sim.model.transports.PrivateTransports.Car
-import pps.covid_sim.model.transports.PublicTransports.{Bus, Train}
+import pps.covid_sim.model.transports.PublicTransports.{Bus, Carriage, Train}
 import pps.covid_sim.util.time.HoursInterval
 
 class TransportsTest {
 
   val bus: Bus = Bus(3, new HoursInterval(15, 16))
   val train: Train = Train(2, new HoursInterval(15, 16))
-  val car: Car = Car(4)
+  val car: Car = Car(2)
 
   val marco: Single = Single(new Person {})
   val lorenzo: Single = Single(new Person {})
   val gianmarco: Single = Single(new Person {})
   val nicolas: Single = Single(new Person {})
+
+  val commuters: List[Single] = (1 to 40).map(e=> Single(new Person {})).toList
 
   @Test
   def testBusTurnout(): Unit = {
@@ -45,6 +49,63 @@ class TransportsTest {
     assertEquals(None, bus.enter(lorenzo))
     assertEquals(Option(bus), bus.enter(gianmarco))
     assertEquals(None, bus.enter(nicolas))
+  }
+
+  @Test
+  def testCarTurnout(): Unit = {
+    assertEquals(Option(car), car.enter(marco))
+    assertEquals(Option(car), car.enter(lorenzo))
+    car.exit(lorenzo)
+    assertEquals(Option(car), car.enter(nicolas))
+    assertEquals(None, car.enter(lorenzo))
+  }
+
+  @Test
+  def testExitFromCarWithNoOneInside(): Unit = {
+    car.exit(lorenzo)
+    assertEquals(Option(car), car.enter(marco))
+    assertEquals(Option(car), car.enter(nicolas))
+    assertEquals(None, car.enter(gianmarco))
+  }
+
+  @Test
+  def testDuplicateEntriesInCar(): Unit = {
+    assertEquals(Option(car), car.enter(lorenzo))
+    assertEquals(None, car.enter(lorenzo))
+    assertEquals(Option(car), car.enter(gianmarco))
+    assertEquals(None, car.enter(nicolas))
+  }
+
+  @Test
+  def testTrainTurnout(): Unit = {
+    enterPeopleFromList(0,commuters.size-2,commuters,train)
+    assertEquals(Option(Carriage(20)), train.enter(marco))
+    assertEquals(Option(Carriage(20)), train.enter(lorenzo))
+    train.exit(lorenzo)
+    assertEquals(Option(Carriage(20)), train.enter(nicolas))
+    assertEquals(None, train.enter(lorenzo))
+  }
+
+  @Test
+  def testExitFromTrainWithNoOneInside(): Unit = {
+    train.exit(lorenzo)
+    enterPeopleFromList(0,commuters.size-2,commuters,train)
+    assertEquals(Option(Carriage(20)), train.enter(marco))
+    assertEquals(Option(Carriage(20)), train.enter(lorenzo))
+    train.exit(marco)
+    assertEquals(Option(Carriage(20)), train.enter(marco))
+  }
+
+  @Test
+  def testDuplicateEntriesInTrain(): Unit = {
+    assertEquals(Option(Carriage(20)), train.enter(lorenzo))
+    assertEquals(None, train.enter(lorenzo))
+    assertEquals(Option(Carriage(20)), train.enter(gianmarco))
+    assertEquals(None, train.enter(gianmarco))
+  }
+
+  def enterPeopleFromList(from: Int, until: Int, listPeople: List[Single], means: Transport ): Unit = {
+    listPeople.slice(from,until) foreach(p => means.enter(p))
   }
 
 }
