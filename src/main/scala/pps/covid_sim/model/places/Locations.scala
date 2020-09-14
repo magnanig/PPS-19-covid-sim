13268d190765f338124a6151bf7f59f78c065cb0
gianmarco <gianmarco.magnani3@studio.unibo.it>
2020-09-14 02:12:45 +0200

    add some useful callbacks in Location trait and implement some methods

--- src/main/scala/pps/covid_sim/model/places/Locations.scala
+++ src/main/scala/pps/covid_sim/model/places/Locations.scala
@@ -9,6 +9,7 @@ import pps.covid_sim.util.time.Time.Day.Day
 object Locations {
 
   trait Location {
+
     private var _currentGroups: Set[Group] = Set()
     private[Locations] var _numCurrentPeople: Int = 0
 
@@ -22,7 +23,23 @@ object Locations {
      * Get the groups that have entered into current location.
      * @return  the set of group inside the current location
      */
-    def currentGroups: Set[Group] = _currentGroups
+    def currentGroups: Set[Group] = synchronized {
+      _currentGroups
+    }
+
+    /**
+     * Check whether current location is open in the specified day or not.
+     * @param day   the desired day
+     * @return      true if location is open at day, false otherwise
+     */
+    def isOpen(day: Day): Boolean = true
+
+    /**
+     * Check whether current location is open between the specified dates interval or not.
+     * @param datesInterval   the desired dates interval
+     * @return                true if location is open between the dates interval, false otherwise
+     */
+    def isOpen(datesInterval: DatesInterval): Boolean = true
 
     /**
      * Lets the specified group enter to current location at the specified time, if it is possible.
@@ -30,13 +47,84 @@ object Locations {
      * @param time    the desired time
      * @return        the optional location (e.g. the room) where group has been entered
      */
-    def enter(group: Group, time: Calendar): Option[Location]
+    final def enter(group: Group, time: Calendar): Option[Location] = synchronized {
+      if (_currentGroups.contains(group)) {
+        println(s"WARNING: ${group.leader} Already entered in the ${getClass.getSimpleName}!")
+      }
+      if(canEnter(group, time)) preEnter(group, time) match {
+        case location @ Some(_) => onEntered(group); location
+        case _ => println(s"WARNING: $group cannot enter to the ${getClass.getSimpleName} at ${time.getTime}"); None
+      } else {
+        None
+      }
+    }
+
+    /**
+     * This method can be overridden to do all operations and/or controls in order to check
+     * permission for group to enter in current location and set other information.
+     * The default implementation doesn't do nothing.
+     * @param group   the group that wants to enter
+     * @param time    the desired time
+     * @return        the optional location (e.g. the room) where group has been entered
+     */
+    protected def preEnter(group: Group, time: Calendar): Option[Location] = Some(this)
+
+    /**
+     * This method gets called after a group has been allowed to enter in current location.
+     * The default implementation, adds group to the set of group entered in location and
+     * updates the number of people as well. So, if you want to override this method, remember
+     * to call "super" before.
+     * @param group   the group that has been entered
+     */
+    protected def onEntered(group: Group): Unit = {
+      _currentGroups += group
+      _numCurrentPeople = _numCurrentPeople + group.size
+    }
 
     /**
      * Lets group exit from current location.
      * @param group   the group that wants to exit
      */
-    def exit(group: Group): Unit
+    final def exit(group: Group): Unit = synchronized {
+      preExit(group)
+      //println(s"${group.leader} Exited from ${getClass.getSimpleName}!")
+      _currentGroups -= group
+      _numCurrentPeople = _numCurrentPeople - group.size
+    }
+
+    /**
+     * In this method it is possible to specify all operations that must be done before
+     * group exits from location. Default implementation is empty.
+     * @param group   the group that is going to exit
+     */
+    protected def preExit(group: Group): Unit = {}
+
+    /**
+     * Propagates virus inside the current location. The default implementation propagates the
+     * virus inside each group, trying to infect every pair of people where neither of them
+     * is keeping safety distance from the other. Furthermore, each person looks for his/her friends
+     * and, if present, may infect them as well. Subclasses can override this method as needed.
+     * @param time  current time
+     * @param place current place
+     */
+    def propagateVirus(time: Calendar, place: Place): Unit = { /* TODO */ }
+
+    /**
+     * Checks whether group can enter to current location at the specified time.
+     * @param group the group that wants to enter
+     * @param time    the desired time
+     * @return        true if group can enter to location at time, false otherwise
+     */
+    protected[places] def canEnter(group: Group, time: Calendar): Boolean = true
+
+    /**
+     * Clear current location, removing all groups.
+     */
+    def clear(): Unit = synchronized {
+      _numCurrentPeople = 0
+      _currentGroups = Set()
+    }
+
   }
 
   trait LimitedPeopleLocation extends Location {
@@ -45,6 +133,12 @@ object Locations {
      * The maximum location's capacity, in term of people that can be contained at the same time.
      */
     val capacity: Int
+
+    override protected[places] def canEnter(group: Group, time: Calendar): Boolean = capacity - _numCurrentPeople >= group.size
+
+    override protected def preEnter(group: Group, time: Calendar): Option[LimitedPeopleLocation] = {
+      if(canEnter(group, time)) Some(this) else None
+    }
 
   }
 
