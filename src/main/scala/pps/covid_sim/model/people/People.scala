5dedace2ce572ffe3050965d59788c9631a36824
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2020-09-26 17:02:53 +0200

    Workers specialization (to manage their primary importance)

--- src/main/scala/pps/covid_sim/model/people/People.scala
+++ src/main/scala/pps/covid_sim/model/people/People.scala
@@ -11,12 +11,19 @@ object People {
 
   trait Employed extends Person {
     private var _workPlace: WorkPlace[_ <: Location] = _
+    private var _primaryImportance: Boolean = false
 
     def setWorkPlace(workPlace: WorkPlace[_ <: Location]): Unit = {
       _workPlace = workPlace
     }
 
     def workPlace: WorkPlace[_ <: Location] = _workPlace
+
+    def setPrimaryImportance(isOfPrimaryImportance: Boolean): Unit = {
+      _primaryImportance = isOfPrimaryImportance
+    }
+
+    def primaryImportance: Boolean = _primaryImportance
   }
 
   case class Student(override val birthDate: Calendar, override val residence: City) extends Person {
@@ -39,12 +46,30 @@ object People {
     override def toString: String = "Worker"
   }
 
+  case class DeliveryMan(override val birthDate: Calendar, override val residence: City) extends Employed {
+    override def toString: String = "Delivery Man"
+    setPrimaryImportance(true)
+  }
+
+  case class SupermarketEmployee(override val birthDate: Calendar, override val residence: City) extends Employed {
+    override def toString: String = "Supermarket Employee"
+    setPrimaryImportance(true)
+  }
+
+  case class HospitalWorker(override val birthDate: Calendar, override val residence: City) extends Employed {
+    override def toString: String = "Hospital Worker (Doctor or Nurse)"
+    setPrimaryImportance(true)
+  }
+
+  case class Pharmacist(override val birthDate: Calendar, override val residence: City) extends Employed {
+    override def toString: String = "Pharmacist"
+    setPrimaryImportance(true)
+  }
 
   case class Teacher(override val birthDate: Calendar, override val residence: City) extends Employed {
     override def toString: String = "Teacher"
   }
-
-
+  
   case class Unemployed(override val birthDate: Calendar, override val residence: City) extends Person {
     override def toString: String = "Unemployed"
   }
