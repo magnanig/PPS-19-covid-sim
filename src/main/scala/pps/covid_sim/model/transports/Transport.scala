991ac637fca159f8d4bf81e182698e78ca94d978
Marco Meluzzi <marco.meluzzi@studio.unibo.it>
2020-09-26 17:09:59 +0200

    Refactor in Transport

--- src/main/scala/pps/covid_sim/model/transports/Transport.scala
+++ src/main/scala/pps/covid_sim/model/transports/Transport.scala
@@ -2,6 +2,7 @@ package pps.covid_sim.model.transports
 
 import java.util.Calendar
 
+import pps.covid_sim.model.clinical.VirusPropagation.tryInfect
 import pps.covid_sim.model.people.PeopleGroup.{Group, Single}
 import pps.covid_sim.model.people.Person
 import pps.covid_sim.model.places.Locations.LimitedPeopleLocation
@@ -32,28 +33,6 @@ trait Transport extends LimitedPeopleLocation {
   override def propagateVirus(time: Calendar, place: Place): Unit = {
     super.propagateVirus(time, place)
     extraGroupVirusPropagation(place, time)
-  }
-
-  // TODO: to be moved in the future (method in common with close locations...)
-  /**
-   *
-   * @param p1
-   * @param p2
-   * @param place
-   * @param time
-   * @return the person who has been infected
-   */
-  def tryInfect(p1: Person, p2: Person, place: Place, time: Calendar): Unit = {
-    if (p1.canInfect != p2.canInfect) {
-      val infectedPerson = if (p1.canInfect) p1 else p2
-      val healthyPerson = if (infectedPerson eq p1) p2 else p1
-
-      val contagionProbability = 0.1 // TODO: to change considering the presence of a mask
-      if (new Random().nextDouble() <= contagionProbability) {
-        println(Some(healthyPerson))
-      }
-
-    }
   }
 
 }
