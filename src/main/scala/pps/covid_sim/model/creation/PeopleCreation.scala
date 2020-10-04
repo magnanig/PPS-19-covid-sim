82e7f24b1d61d69cc09ab67fbe1779c5a902fef0
Nicolas Pasolini <nicolas.pasolini@studio.unibo.it>
2020-10-04 13:57:50 +0200

    Code refactoring in creation package

---
+++ src/main/scala/pps/covid_sim/model/creation/PeopleCreation.scala
@@ -0,0 +1,48 @@
+package pps.covid_sim.model.creation
+
+import pps.covid_sim.model.container.CitiesContainer
+import pps.covid_sim.model.people.People.{Student, Teacher, Unemployed, Worker}
+import pps.covid_sim.model.people.Person
+import pps.covid_sim.model.places.Locality.City
+import pps.covid_sim.parameters.CreationParameters
+import pps.covid_sim.parameters.CreationParameters.{ studentsPercentage,
+  teachersPercentage, unemployedPercentage, workersPercentage}
+import pps.covid_sim.util.RandomGeneration.randomBirthDate
+import pps.covid_sim.util.Statistic
+
+private[creation] abstract class PeopleCreation {
+
+  var people: List[Person] = List()
+  val _cities: Set[City]
+
+  def create(): List[Person] = {
+    CitiesContainer.add(_cities)
+    people = _cities.toList.flatMap(city => createPeople(city))
+    people
+  }
+
+  def createPeople(city: City): List[Person] = {
+    var people: List[Person] = List()
+    val number: List[Int] = computeProportions(city)
+    val childrenAndElderly: List[Int] = Statistic.totalPercentageToInt(number(3), 50, 50)
+
+    (1 to number.head).foreach(_ => people = people :+ Worker(randomBirthDate(23, 68), city))
+    (1 to number(1)).foreach(_ => people = people :+ Teacher(randomBirthDate(28, 68), city))
+    (1 to number(2)).foreach(_ => people = people :+ Student(randomBirthDate(6, 24), city))
+    (1 to childrenAndElderly.head).foreach(_ => people = people :+ Unemployed(randomBirthDate(0, 5), city))
+    (1 to childrenAndElderly.last).foreach(_ => people = people :+ Unemployed(randomBirthDate(69, 100), city))
+
+    people
+  }
+
+  private def computeProportions(city: City): List[Int] = {
+    if (city.numResidents > CreationParameters.minResidencesToSchool) {
+      Statistic.totalPercentageToInt(city.numResidents,
+        workersPercentage, teachersPercentage, studentsPercentage, unemployedPercentage)
+    } else {
+      Statistic.totalPercentageToInt(city.numResidents,
+        workersPercentage + teachersPercentage, 0, 0, unemployedPercentage + studentsPercentage)
+    }
+  }
+
+}
--- src/main/scala/pps/covid_sim/model/creation/province/ProvincePeopleCreation.scala
+++ src/main/scala/pps/covid_sim/model/creation/province/ProvincePeopleCreation.scala
@@ -1,14 +1,8 @@
 package pps.covid_sim.model.creation.province
 
-import pps.covid_sim.model.container.CitiesContainer
-import pps.covid_sim.model.creation.region.RegionCitiesCreation
-import pps.covid_sim.model.people.People.{Student, Teacher, Unemployed, Worker}
+import pps.covid_sim.model.creation.PeopleCreation
 import pps.covid_sim.model.people.Person
 import pps.covid_sim.model.places.Locality.{City, Province}
-import pps.covid_sim.parameters.CreationParameters
-import pps.covid_sim.parameters.CreationParameters.{studentsPercentage, teachersPercentage, unemployedPercentage, workersPercentage}
-import pps.covid_sim.util.RandomGeneration.randomBirthDate
-import pps.covid_sim.util.Statistic
 
 private[province] object ProvincePeopleCreation {
 
@@ -24,39 +18,8 @@ private[province] object ProvincePeopleCreation {
 
 }
 
-private class ProvincePeopleCreation(val province: Province) {
+private class ProvincePeopleCreation(val province: Province) extends PeopleCreation {
 
-  private val _cities: Set[City] = ProvinceCitiesCreation.create(province)
-  var people: List[Person] = List()
-
-  def create(): List[Person] = {
-    CitiesContainer.add(_cities)
-    people = _cities.toList.flatMap(city => createPeople(city))
-    people
-  }
-
-  def createPeople(city: City): List[Person] = {
-    var people: List[Person] = List()
-    val number: List[Int] = computeProportions(city)
-    val childrenAndElderly: List[Int] = Statistic.totalPercentageToInt(number(3), 50, 50)
-
-    (1 to number.head).foreach(_ => people = people :+ Worker(randomBirthDate(23, 68), city))
-    (1 to number(1)).foreach(_ => people = people :+ Teacher(randomBirthDate(28, 68), city))
-    (1 to number(2)).foreach(_ => people = people :+ Student(randomBirthDate(6, 24), city))
-    (1 to childrenAndElderly.head).foreach(_ => people = people :+ Unemployed(randomBirthDate(0, 5), city))
-    (1 to childrenAndElderly.last).foreach(_ => people = people :+ Unemployed(randomBirthDate(69, 100), city))
-
-    people
-  }
-
-  private def computeProportions(city: City): List[Int] = {
-    if (city.numResidents > CreationParameters.minResidencesToSchool) {
-      Statistic.totalPercentageToInt(city.numResidents,
-        workersPercentage, teachersPercentage, studentsPercentage, unemployedPercentage)
-    } else {
-      Statistic.totalPercentageToInt(city.numResidents,
-        workersPercentage + teachersPercentage, 0, 0, unemployedPercentage + studentsPercentage)
-    }
-  }
+  override val _cities: Set[City] = ProvinceCitiesCreation.create(province)
 
 }
